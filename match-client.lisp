d50dd34cf8e4eebc25c7a2fcf379629f16235e3c
Timofei Shatrov <timofei.shatrov@gmail.com>
2017-06-12 15:11:31 +0300

    add/delete wip

--- match-client.lisp
+++ match-client.lisp
@@ -1,4 +1,44 @@
 (in-package #:match-client)
 
-(load (asdf:system-relative-pathname :ichiran "settings.lisp") :if-does-not-exist nil)
+(defvar *base-url* "")
+(defvar *local-tag* "")
+(defvar *cache-file* "")
+(defvar *root-dirs* '())
 
+(load (asdf:system-relative-pathname :match-client "settings.lisp") :if-does-not-exist nil)
+
+(defun api-url (path)
+  (concatenate 'string *base-url* path))
+
+(defun get-path (path &key (use-tag t))
+  (format nil "~@[[~a] ~]~a" (and use-tag *local-tag*) path))
+
+(defun parse-request (&rest args)
+  (multiple-value-bind (content return-code)
+      (apply 'drakma:http-request args)
+    (cond
+      ((<= 400 return-code 499)
+       (jsown:new-js
+         ("status" "fail")
+         ("error" content)
+         ("code" return-code)))
+      (t (let ((obj (jsown:parse content)))
+           (jsown:extend-js obj ("code" return-code)))))))
+
+(defun add-local (file &key path (use-tag t) (metadata "{}"))
+  "Add local image to Match server"
+  (parse-request
+   (api-url "/add")
+   :method :post
+   :form-data t
+   :parameters `(("image" . ,(pathname file))
+                 ("filepath" . ,(get-path (or path file) :use-tag use-tag))
+                 ("metadata" . ,metadata))))
+
+(defun delete-path (path &key (use-tag t))
+  (parse-request
+   (api-url "/delete")
+   :method :delete
+   :form-data t
+   :parameters `(("filepath" . ,(get-path path :use-tag use-tag)))
+   ))
