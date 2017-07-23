a9fb65cd18d1ee62ec97459a35b5a3cf7506d5c7
Timofei Shatrov <timofei.shatrov@gmail.com>
2017-07-23 18:21:10 +0300

    handle files with unicode paths that crash GraphicsMagick

--- image.lisp
+++ image.lisp
@@ -5,7 +5,14 @@
 (defun resize-image (input-path output-path
                      &key (max-width *max-dimension*) (max-height *max-dimension*)
                        (filter :%QuadraticFilter) (blur 1))
-  (gm::with-magick-wand (wand :input-path input-path)
+  (gm::with-magick-wand (wand)
+    (handler-case (gm::%MagickReadImage wand input-path)
+      ;; graphicsmagick cannot read Unicode filenames on Windows so attempt to load a copy
+      (gm::magick-error ()
+        (uiop:with-temporary-file (:pathname tmp :prefix "gm" :type (pathname-type input-path))
+          (uiop:copy-file input-path tmp)
+          (setf wand (gm::%NewMagickWand))
+          (gm::%MagickReadImage wand (namestring tmp)))))
     (let ((w (gm::%MagickGetImageWidth wand))
           (h (gm::%MagickGetImageHeight wand))
           (res nil))
