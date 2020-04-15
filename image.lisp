0e968859397969bc7209b5706f64f5e8f03faaf6
Timofei Shatrov <timofei.shatrov@gmail.com>
2020-04-16 01:43:47 +0300

    blacklist animated webp images

--- image.lisp
+++ image.lisp
@@ -2,17 +2,25 @@
 
 (defvar *max-dimension* 2048)
 
+
+(define-condition invalid-image (error)
+  ((path :reader path :initarg :path))
+  (:report (lambda (c s) (format s "~S is unreadable by Graphicsmagick" (path c)))))
+
 (defun resize-image (input-path output-path
                      &key (max-width *max-dimension*) (max-height *max-dimension*)
                        (filter :%QuadraticFilter) (blur 1))
   (gm::with-magick-wand (wand)
     (handler-case (gm::%MagickReadImage wand input-path)
       ;; graphicsmagick cannot read Unicode filenames on Windows so attempt to load a copy
-      (gm::magick-error ()
-        (uiop:with-temporary-file (:pathname tmp :prefix "gm" :type (pathname-type input-path))
-          (uiop:copy-file input-path tmp)
-          (setf wand (gm::%NewMagickWand))
-          (gm::%MagickReadImage wand (namestring tmp)))))
+      (gm::magick-error (c)
+        (case (gm::code c)
+          (430 (uiop:with-temporary-file (:pathname tmp :prefix "gm" :type (pathname-type input-path))
+                 (uiop:copy-file input-path tmp)
+                 (setf wand (gm::%NewMagickWand))
+                 (gm::%MagickReadImage wand (namestring tmp))))
+          (450 (error 'invalid-image :path input-path))
+          (t (error c)))))
     (let ((w (gm::%MagickGetImageWidth wand))
           (h (gm::%MagickGetImageHeight wand))
           (res nil))
@@ -22,4 +30,3 @@
           (gm::%MagickWriteImage wand output-path)
           (setf res output-path))
         (values res w h fw fh)))))
-    
