108cda708ca9b43a266c177141960fa46e5378d4
Timofei Shatrov <timofei.shatrov@gmail.com>
2020-04-25 22:58:53 +0300

    sorter 1

--- viewer.lisp
+++ viewer.lisp
@@ -31,3 +31,37 @@
       (view-file-native file))
   #-swank
   (view-file-native file))
+
+
+
+
+(defun generate-map (target)
+  (setf target (uiop:ensure-directory-pathname target))
+  (loop for i from 0
+     for dir in (sort (uiop:subdirectories target) 'string-lessp :key 'namestring)
+     for dirname = (car (last (pathname-directory dir)))
+     collect (list (princ-to-string i) dirname dir)))
+
+(defun print-choices (choices &optional s)
+  (format s "~%~<~@{~<[~a] ~a~:>~^  ~}~:@>" choices))
+
+(defun choice-map (map &aux (special '(("s" "skip") ("q" "quit"))))
+  (print-choices map t)
+  (print-choices special t)
+  (format t "~%Select target directory: ")
+  (loop for choice = (read-line)
+     for ass = (assoc choice map :test 'equalp)
+     if (equalp choice "q") do (return :quit)
+     else if (equalp choice "s") do (return :skip)
+     else if ass do (return (third ass))
+     else do (format t "~%Invalid choice, try again: ")))
+
+(defun sort-dir (source &key map target)
+  (unless target
+    (setf target source))
+  (setf source (uiop:ensure-directory-pathname source)
+        target (uiop:ensure-directory-pathname target))
+  (unless map
+    (setf map (generate-map target)))
+
+  (loop for image in (sort (find-images source) '< :key 'mtime)
