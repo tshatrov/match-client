5f124ce0027f7c3d1431dc8e57a479be45501b10
Timofei Shatrov <timofei.shatrov@gmail.com>
2020-05-02 19:53:18 +0300

    reduce the number of eval-in-emacs calls

--- viewer.lisp
+++ viewer.lisp
@@ -13,16 +13,16 @@
 (defun view-file-slime (file &key (bufname "*image-viewer*"))
   "Requires setting `slime-enable-evaluate-in-emacs' to T in Emacs"
   (let ((ns (namestring file)))
-    (swank::eval-in-emacs `(slime-with-popup-buffer (,bufname :connection t :package t)
-                             (insert-image (create-image ,ns))
-                             (image-mode)
-                             (setf buffer-file-name ,ns)
-                             (not-modified)
-                             (image-toggle-display-image)
-                             ))
-    ;; try to resize the image after the buffer is displayed
-    (swank::eval-in-emacs `(with-current-buffer ,bufname (image-toggle-display-image)))
-    ))
+    (swank::eval-in-emacs
+     `(progn
+        (slime-with-popup-buffer (,bufname :connection t :package t)
+          (insert-image (create-image ,ns))
+          (image-mode)
+          (setf buffer-file-name ,ns)
+          (not-modified)
+          (image-toggle-display-image))
+        ;; try to resize the image after the buffer is displayed
+        (with-current-buffer ,bufname (image-toggle-display-image))))))
 
 (defun view-file (file)
   #+swank
