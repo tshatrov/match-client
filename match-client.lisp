fc75bce02a588b4bdf1f6fe3aa5246c2e13dd3b7
Timofei Shatrov <timofei.shatrov@gmail.com>
2020-07-26 02:40:19 +0300

    add working docker-compose config

--- match-client.lisp
+++ match-client.lisp
@@ -192,7 +192,7 @@
                   (setf (status file) :error (message file) result)
                   (format-msg "Error deleting ~a: ~a" filepath result)))))))))
 
-(defun update (&key (threads 2))
+(defun update (&key (threads 4))
   (format-msg "Updating cache...")
   (force-output)
   (setf *cache* (update-cache))
